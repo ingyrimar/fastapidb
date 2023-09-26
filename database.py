7518e37962f09e7d90919d7fe1298d6e86485647
ingyrimar <ingyrimar2023@gmail.com>
2023-09-26 17:10:40 -0400

    agrege una nueva ruta en database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:1234@0.0.0.0/item_db",
+engine = create_engine("postgresql://item_db_77cq_user:pmKeR0xcazJ4IvHj1mMn0gKMqOJGBszI@dpg-ck8t44c7m7is73aki8kg-a.ohio-postgres.render.com/item_db_77cq",
           echo=True             
         )
 
