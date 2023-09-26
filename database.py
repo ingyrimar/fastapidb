e6809dee47fb969fb849a658add11c8b9a21676d
ingyrimar <ingyrimar2023@gmail.com>
2023-09-26 16:03:13 -0400

    agrege la direccion 0.0.0.0

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:1234@localhost/item_db",
+engine = create_engine("postgresql://postgres:1234@0.0.0.0/item_db",
           echo=True             
         )
 
