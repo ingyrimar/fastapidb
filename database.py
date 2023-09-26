95a013ca18b1da689ac36f12a5402585bd8c7fcb
ingyrimar <ingyrimar2023@gmail.com>
2023-09-25 23:09:15 -0400

    modifique de nuevo database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgres://item_db_77cq_user:pmKeR0xcazJ4IvHj1mMn0gKMqOJGBszI@dpg-ck8t44c7m7is73aki8kg-a.ohio-postgres.render.com/item_db_77cq",
+engine = create_engine("postgresql://postgres:1234@localhost/item_db",
           echo=True             
         )
 
